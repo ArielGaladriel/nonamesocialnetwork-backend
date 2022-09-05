5cf9d934e17e63ac4026181093b98332d55f7017
ArielGaladriel <arielbagdasarov@gmail.com>
2022-09-05 17:23:58 +0400

    pre-alpha 0.0.14: followers are fixed

--- src/followers/serializers.py
+++ src/followers/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from .models import Follower
+from ..usersprofile.models import UsersProfile
+
+
+class UsersNameSerializer(serializers.ModelSerializer):
+    """
+    """
+
+    class Meta:
+        model = UsersProfile
+        fields = ['id','username']
 
 
 class FollowersListSerializer(serializers.ModelSerializer):
     """
     """
+    subscriber = UsersNameSerializer()
 
     class Meta:
         model = Follower
--- src/followers/views.py
+++ src/followers/views.py
@@ -11,7 +11,7 @@ class FollowersListView(generics.ListAPIView):
     serializer_class = FollowersListSerializer
 
     def get_queryset(self):
-        return Follower.objects.filter(user=self.request.user)
+        return Follower.objects.filter(user=self.kwargs['pk'])
 
 
 class AddFollowerView(views.APIView):
@@ -25,14 +25,20 @@ class AddFollowerView(views.APIView):
             user = UsersProfile.objects.get(id=pk2)
         except Follower.DoesNotExist:
             return response.Response(status=404)
-        if not Follower.objects.filter(subscriber=request.user, user=user).exists() and request.user.id != pk2:
+        if not Follower.objects.filter(subscriber=request.user, user=user).exists() and request.user.id != pk2 and pk == request.user.id:
             Follower.objects.create(subscriber=request.user, user=user)
-        return response.Response(status=201)
+            return response.Response(status=201)
+        else:
+            return response.Response(status=403)
 
     def delete(self, request, pk, pk2):
         try:
             sub = Follower.objects.get(subscriber=request.user, user=pk2)
         except Follower.DoesNotExist:
             return response.Response(status=404)
-        sub.delete()
-        return response.Response(status=204)
+        if pk == request.user.id:
+            sub.delete()
+            return response.Response(status=204)
+        else:
+            return response.Response(status=403)
+
